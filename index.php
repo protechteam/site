fd7f802d8bd33c43706e39646901ae2065da8dfa
protechteam <93289793+protechteam@users.noreply.github.com>
2021-10-27 16:39:39 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming Soon...</title>
+</head>
+<body>
+
+    <center>
+        <div>
+            <img src="pro.png">
+        </div></br>
+        <h1>‌‌( IT ) ئێمە وەکوو تیمێکی شارەزا لە بواری</br>
+         إن شاء الله بەم زوانە هەڵ دەستین بە کردنەوەی کۆڕس لە زمانەکانی پرۆگرام سازی</br> بە سەرپەرشتی کەسانی شارەزاو لێهاتوو لەو بوارەدا</h1>
+    </center>
+    
+</body>
+</html>
+
+<style>
+
+    *{
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-image: url("https://www.teahub.io/photos/full/8-80447_blurred-wallpaper-4k.jpg");
+        color: white;
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 100vh;
+    }
+
+    h1{
+        font-size: 17px;
+        font-family: sans-serif;
+        font-weight: lighter;
+    }
+
+    div{
+        margin-top: 10px;
+        display: flex;
+        width: 310px;
+        height: 300px;
+        border-radius: 50%;
+        background-color: white;
+        align-items: center;
+        overflow: hidden;
+        box-shadow: 0px 1px 15px 5px white;
+    }
+
+    img{
+        display: block;
+        width: 400px;
+        margin-left: -39px;
+    }
+
+    @media only screen and (max-width: 318px){
+
+        div{
+            width: 210px;
+            height: 200px;
+        }
+
+        img{
+            width: 294px;
+        }
+    }
+
+    @media only screen and (max-width: 218px){
+
+        div{
+            width: 190px;
+            height: 183px;
+        }
+
+        img{
+            width: 274px;
+        }
+    }
+
+</style>
